790b82a86ad74656cf43609220322c176567a7c4
hasan <114818012+WARRIORDAG@users.noreply.github.com>
2024-02-27 23:11:15 +0300

    week 11 sapka kapmaca second commit
    üretken akademi week 11 assignment

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using Photon.Realtime;
 
 
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 {
     [HideInInspector]
     public int id;
@@ -25,13 +25,29 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Move();
-
-        if (Input.GetKeyDown(KeyCode.Space))        // space tuşu için koşul oluşturdum
+        if (PhotonNetwork.IsMasterClient)
         {
-            TryJump();
+            if (curHatTime >= GameManager.instance.timeToWin && GameManager.instance.gameEnded)
+            {
+                GameManager.instance.gameEnded = true;
+                GameManager.instance.photonView.RPC("WinGame", RpcTarget.All, id);
+            }
+
         }
 
+        if (photonView.IsMine)
+        {
+            Move();
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TryJump();
+            }
+            if (hatObject.activeInHierarchy)
+            {
+                curHatTime += Time.deltaTime;
+            }
+        }
     }
 
     void Move()     // horizontal ve vertical hareketler için bu fonksiyonu yazdım
@@ -51,4 +67,63 @@ public class PlayerController : MonoBehaviour
         }
 
     }
+
+    public void SetHat (bool hasHat)    // şapka var mı? yokmu için fonksiyon oluşturdum!!
+    {
+        hatObject.SetActive(hasHat);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (GameManager.instance.GetPlayer(collision.gameObject).id == GameManager.instance.playerWithHat)
+            {
+                if (GameManager.instance.CanGetHat())
+                {
+                    GameManager.instance.photonView.RPC("GiveHat", RpcTarget.All, id, false);
+                }
+            }
+        }
+    }
+
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        photonPlayer = player;
+        id = player.ActorNumber;
+        GameManager.instance.players[id - 1] = this;
+
+        if (id ==1)
+        {
+            GameManager.instance.GiveHat(id, true);
+
+        }
+            
+
+        //GameManager.instance.players[id -1] = this;
+
+        if (!photonView.IsMine)
+        {
+            rig.isKinematic = true;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)      // zamanı senkronize etmek için yazdım
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(curHatTime);
+        }
+        else if (stream.IsReading)
+        {
+            curHatTime = (float)stream.ReceiveNext();
+        }
+    }
 }
+
+
